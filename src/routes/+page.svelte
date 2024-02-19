e35aa69c31c08a86fbbdbf8f20351a844c6d9457
Melvin Lundberg <mellun04@elevad.umea.se>
2024-02-19 12:00:41 +0100

    Klar med Start,typ

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,38 @@
 
+<script>
+
+  let x = 1
+  
+</script>
+
 <h1>
-  Välkommen till min hemsida!
+  Välkommen till Melvins hemsida!
 </h1>
 
-<h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
+<h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 3 på Maja Beskowgymnasiet!</h2>
 
-<h3 >Förnamn Efternamn</h3>
+<h3 class="name" >Melvin är en stor pojke</h3>
 
+<h3> {x} </h3>
+
+<h3 id="inspo">
+  "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code." – Dan Salomon
+</h3>
+
+<style>
+  
+  h1{
+    color: goldenrod;
+  }
+
+  .name{
+    color: rgb(32, 158, 162);
+    font-family:'Comic Sans MS'
+  }
+  #inspo{
+    color:brown;
+    font-size: 160%;
+  }
+
+
+</style>
